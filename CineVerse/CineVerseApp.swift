53ca0d97c390544e0451546ccd5f3805b19b5d42
Pankaj Kumar Rana <pankajkumarrana0611@gmail.com>
2026-01-10 23:22:37 +0530

    Initial Commit

---
+++ CineVerse/CineVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineVerseApp.swift
+//  CineVerse
+//
+//  Created by Pankaj Kumar Rana on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CineVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineVerse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineVerse
+//
+//  Created by Pankaj Kumar Rana on 10/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
